4fb18f60b3153cf8e63f06c225cd5d77d87b48c9
Anddd7 <liaoad_sapce@sina.com>
2021-03-05 15:06:28 +0800

    wrap with coroutine

--- src/apiTest/kotlin/com/github/anddd7/core/transactional/FakeComponents.kt
+++ src/apiTest/kotlin/com/github/anddd7/core/transactional/FakeComponents.kt
@@ -2,6 +2,8 @@ package com.github.anddd7.core.transactional
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
@@ -28,41 +30,81 @@ class HubService(
 ) {
     @Transactional
     fun create(name1: String, name2: String, name3: String): HubModel {
-        val part1 = part1Dao.save(Part1(name = name1)).also(et::isTriggeredAt1)
-        val part2 = part2Dao.save(Part2(name = name2)).also(et::isTriggeredAt2)
-        val part3 = part3Dao.save(Part3(name = name3)).also(et::isTriggeredAt3)
+        return runBlocking {
+            val deferredPart1 = async { part1Dao.save(Part1(name = name1)).also(et::isTriggeredAt1) }
+            val deferredPart2 = async { part2Dao.save(Part2(name = name2)).also(et::isTriggeredAt2) }
+            val deferredPart3 = async { part3Dao.save(Part3(name = name3)).also(et::isTriggeredAt3) }
 
-        val entity = repository.save(HubPO(id = 0, part1 = part1.id, part2 = part2.id, part3 = part3.id))
-            .also(et::isTriggeredAt4)
+            val part1 = deferredPart1.await()
+            val part2 = deferredPart2.await()
+            val part3 = deferredPart3.await()
 
-        return HubModel(entity.id, part1, part2, part3)
+            val entity = repository.save(
+                HubPO(
+                    id = 0,
+                    part1 = part1.id,
+                    part2 = part2.id,
+                    part3 = part3.id
+                )
+            )
+                .also(et::isTriggeredAt4)
+
+            HubModel(entity.id, part1, part2, part3)
+        }
     }
 
     @Transactional
     fun update(hub: HubModel): HubModel {
-        val part1 = part1Dao.save(hub.part1).also(et::isTriggeredAt1)
-        val part2 = part2Dao.save(hub.part2).also(et::isTriggeredAt2)
-        val part3 = part3Dao.save(hub.part3).also(et::isTriggeredAt3)
+        return runBlocking {
+            val deferredPart1 = async { part1Dao.save(hub.part1).also(et::isTriggeredAt1) }
+            val deferredPart2 = async { part2Dao.save(hub.part2).also(et::isTriggeredAt2) }
+            val deferredPart3 = async { part3Dao.save(hub.part3).also(et::isTriggeredAt3) }
 
-        return HubModel(hub.id, part1, part2, part3)
+
+            val part1 = deferredPart1.await()
+            val part2 = deferredPart2.await()
+            val part3 = deferredPart3.await()
+
+            HubModel(hub.id, part1, part2, part3)
+        }
     }
 
     fun find(id: Long): HubModel {
         val entity = repository.getOne(id)
 
-        val part1 = part1Dao.find(entity.part1)
-        val part2 = part2Dao.find(entity.part2)
-        val part3 = part3Dao.find(entity.part3)
+        return runBlocking {
+            val deferredPart1 = async { part1Dao.find(entity.part1) }
+            val deferredPart2 = async { part2Dao.find(entity.part2) }
+            val deferredPart3 = async { part3Dao.find(entity.part3) }
 
-        return HubModel(entity.id, part1, part2, part3)
+            val part1 = deferredPart1.await()
+            val part2 = deferredPart2.await()
+            val part3 = deferredPart3.await()
+
+            HubModel(id, part1, part2, part3)
+        }
     }
 
     fun findAll(): List<HubModel> {
-        return repository.findAll().map {
-            val part1 = part1Dao.find(it.part1)
-            val part2 = part2Dao.find(it.part2)
-            val part3 = part3Dao.find(it.part3)
-            HubModel(it.id, part1, part2, part3)
+        val hubs = repository.findAll()
+
+        return runBlocking {
+            hubs
+                .map {
+                    val part1 = async { part1Dao.find(it.part1) }
+                    val part2 = async { part2Dao.find(it.part2) }
+                    val part3 = async { part3Dao.find(it.part3) }
+                    it to Triple(part1, part2, part3)
+                }
+                .map {
+                    val (id, deferred) = it
+
+                    val part1 = deferred.first.await()
+                    val part2 = deferred.second.await()
+                    val part3 = deferred.third.await()
+
+                    HubModel(id.id, part1, part2, part3)
+                }
         }
     }
 }
